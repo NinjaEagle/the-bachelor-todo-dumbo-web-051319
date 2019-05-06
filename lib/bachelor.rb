4da2250f63507d63c2eb24ab44100ffe2b09910e
Kevin W <kcwang06@gmail.com>
2019-05-06 20:48:17 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,20 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data.each do |season_number, contestants|
+		if season_number == season
+			contestants.each do |contestant|
+				return contestant["name"].split.first if contestant["status"] == "Winner"
+			end
+		end
+	end
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |season_number, contestants|
+    	contestants.each do |contestant|
+    		return contestant["name"] if contestant["occupation"] == occupation
+    	end
 end
 
 def count_contestants_by_hometown(data, hometown)
